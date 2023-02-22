7462dfe038180e4d2aae64cdd4b7d69b43ff205a
Mandeep0815 <mandeepkaur_1@icloud.com>
2023-02-22 02:54:45 +0000

    Instiantiated n Passed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,7 @@
 
 public class Lab {
+
+
     /**
      * Java relies very heavily on OOP, so all of our code will rely on Objects. Before we write our own objects,
      * let's first look into how we can leverage them in our code. Every test, for instance, has to first create an
@@ -18,6 +20,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Lab l1 = new Lab();
+        return l1;
     }
 }
